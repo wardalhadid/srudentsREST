bbca6d1f37e402b5612894273c108ae9c6fbaf85
ward <wf.hadid@gmail.com>
2023-10-01 15:47:51 -0400

    springboot tutorial

---
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @Test
+    void getStudents() {
+        assertTrue(true);
+    }
+
+    @Test
+    void addNewStudent() {
+        assertTrue(true);
+    }
+
+    @Test
+    void deleteStudent() {
+        assertTrue(true);
+    }
+
+    @Test
+    void updateStudent() {
+        assertTrue(true);
+    }
+}
